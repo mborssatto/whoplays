4b7aa1754421325656651be9470b1c0309968d59
mborssatto <mborssatto@gmail.com>
2023-01-17 11:29:47 +0000

    adds user model

---
+++ backend/models/user.js
@@ -0,0 +1,18 @@
+const event = require('./event')
+
+class User {
+    id
+    favorites = []
+    
+    constructor (name, spotifyId) {
+        this.name = name
+        this.spotifyId = spotifyId
+    }
+
+    addToFavorites(event) {
+        event.favoritedBy.push(this)
+        this.favorites.push(event)
+    }
+}
+
+module.exports = User
