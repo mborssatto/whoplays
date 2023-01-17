1dcdc2771f391ad326cde29a3a8c25835f1e7924
mborssatto <mborssatto@gmail.com>
2023-01-17 11:41:19 +0000

    adds router to get user by ID

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:id', async (req, res, next) => {
+  res.send(await User.findById(req.params.id))
+})
+
 module.exports = router;
