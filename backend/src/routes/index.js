46b16c616136090f56ed2fa06b1461cc8ebb270f
mborssatto <mborssatto@gmail.com>
2023-01-19 09:12:02 +0000

    reviews text in homepage

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const Event = require('../models/event')
+
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res) => {
+  res.render('index', { title: `Mariana's project for Coyotiv's Software Engineering course` })
+})
 
-module.exports = router;
+module.exports = router
