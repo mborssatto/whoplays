d876abe8ca344b27ecde490d5f743ddab7c3330e
mborssatto <mborssatto@gmail.com>
2023-01-20 10:26:39 +0000

    adds information about this site

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is the final project for Coyotiv's Software Engineering course</h1>
+    <h2>Things will probably not behave as expected and there's nothing to worry about! :-)</h2>
   </div>
 </template>
