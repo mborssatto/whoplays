5fd328267fa75afe9c38030807d210370bf0aad0
mborssatto <mborssatto@gmail.com>
2023-01-23 15:52:01 +0000

    removes deprecated /initialize endpoint

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const app = require('../app')
 const Event = require('../models/event')
-
+const request = require('request')
 const router = express.Router()
 
-// get Spotify access token
+// get Spotify access token 
 router.get('/token', async (req, res, next) => {
   res.send(await req.app.get('spotifyAccessToken'))
 })
 
 
-// filter by city or return all events.
+
+// filter by city or return all events. WORKS with url (eg) http://localhost:3000/events?city=Berlin
 router.get('/', async (req, res) => {
   let result
 
@@ -19,41 +20,6 @@ router.get('/', async (req, res) => {
   } else result = await Event.find()
 
   return res.render('events', {result})
-})
-
-//Initialize (refactor out)
-router.get('/initialize', async (req, res) => {
-  const eventOne = await Event.create({
-    name: 'Festival One',
-    artists: ['Maria Bethânia', 'Caetano Veloso'],
-    date: '2022-11-01',
-    venue: 'HKW',
-    city: 'Berlin',
-  })
-  const eventTwo = await Event.create({
-    name: 'Festival Two',
-    artists: ['Mulatu Astatke'],
-    date: '2022-12-01',
-    venue: 'HKW',
-    city: 'Berlin',
-  })
-  const eventThree = await Event.create({
-    name: 'Festival Three',
-    artists: ['Gal Costa'],
-    date: '2023-10-01',
-    venue: 'Teatro Central',
-    city: 'São Paulo',
-  })
-
-  // MOCK User input
-  const dateRangeStart = '2022-11-01'
-  const dateRangeEnd = '2022-12-08'
-  const city = 'Berlin'
-
-  console.log(eventOne.detail)
-  console.log(eventTwo)
-
-  res.sendStatus(200)
 })
 
 // get events by ID
@@ -78,10 +44,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-
-
 // Add events endpoint
-
 router.post('/add', async (req, res) => {
   try {
     const createdEvents = await Event.create(req.body);
