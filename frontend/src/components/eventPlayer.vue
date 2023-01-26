6072814c7a7e8bedb6ed68cb996318d672e1dc38
mborssatto <mborssatto@gmail.com>
2023-01-26 09:02:38 +0000

    adds an EventPlayer in the EventView (mvp)

---
+++ frontend/src/components/eventPlayer.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+//- p {{ event.bodyParsed.artists.items[0].id }}
+b-card.mb-2(:title='event.name' style='max-width: 20rem;')
+  b-card-text
+    h5 {{ event.artists.join(", ") }}
+    p {{ new Intl.DateTimeFormat('de-DE').format(new Date(event.date)) }}
+    p {{ event.city }}
+    p {{ event.venue }}
+  b-button(v-if="event", href="", variant='primary') Create Playlist
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'EventPlayer',
+  props: ['event']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.card-body {
+  justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+}
+
+.btn {
+  align-self: center
+}
+</style>
--- frontend/src/views/EventView.vue
+++ frontend/src/views/EventView.vue
@@ -1,18 +1,17 @@
 <template lang="pug">
 div
     iframe(:src="'https://open.spotify.com/embed/artist/' + eventData.bodyParsed.artists.items[0].id + '?utm_source=generator&theme=2'")
-    //- iframe(style='border-radius:12px' :src="'https://open.spotify.com/embed/artist/' + eventData.bodyParsed.artists.items[0].id + '?utm_source=generator&theme=0'" width='100%' height='152' frameborder='0' allowfullscreen='' clipboard-write; encrypted-media;picture-in-picture' loading='lazy')
-    event-card(:event='eventData.event' v-if='eventData.event')
+    event-player(:event='eventData.event' v-if='eventData.event')
 </template>
 
 <script>
 import axios from 'axios'
-import EventCard from "@/components/eventCard.vue";
+import EventPlayer from "@/components/eventPlayer.vue";
 
 export default {
     name: "EventView",
     components: {
-        'event-card': EventCard,
+        'event-player': EventPlayer,
     },
     data() {
         return {
