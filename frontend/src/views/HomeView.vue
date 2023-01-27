b4725b9782fabe2de90b678dd557ee98dfc52634
mborssatto <mborssatto@gmail.com>
2023-01-27 19:42:35 +0000

    adds city filter to home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template lang="pug">
 .home
   h1 Welcome to whoPlays
-  h2 Check out these events!
+  h2 Find events in your city and get to know the lineup.
+  b-container
+    b-form-select(v-model="selectedCity" @change="filterEvents")
+      option(value="") Select your city
+      option(v-for="city in allCities" :value="city") {{ city }}
+    //- b-form-datepicker(v-model="selectedDate"  style='margin: auto')
   b-container
     b-row(align-v='stretch')
       b-col.card-row(md='4' v-for='event in events')
@@ -15,18 +20,29 @@ import EventCard from "@/components/eventCard.vue";
 export default {
   name: "Home",
   components: {
-    'event-card' : EventCard,
+    'event-card': EventCard,
   },
   data() {
     return {
-      events: []
+      events: [],
+      selectedCity: '',
+      allCities: []
+      // selectedDate: ''
     }
   },
   async created() {
     const eventsRequest = await axios.get('/events')
     this.events = eventsRequest.data
-  }
-};
+    this.allCities = [...new Set(this.events.map(event => event.city))]// Extract unique city names from the events array
+  },
+  methods: {
+    async filterEvents() {
+      const eventsRequest = await axios.get(`/events?city=${this.selectedCity}`)
+      // const eventsRequest = await axios.get(`/events?city=${this.selectedCity}&date=${this.selectedDate}`)
+      this.events = eventsRequest.data
+    }
+  },
+}
 </script>
 
 <style>
