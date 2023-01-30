7e815b0b2dd2d2460a198880d259a075eed34938
mborssatto <mborssatto@gmail.com>
2023-01-30 09:59:15 +0000

    adds information about the project

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -2,6 +2,20 @@
 .about
   h1 This is the final project for 
   h2 Coyotiv's Software Engineering course
-  p
-    | Things will probably not behave as expected and there's nothing to worry about! :-)
+  div.description 
+    p
+      | This application consists of a list of imaginary events that you can filter by city and listen to Spotify music previews of real artists.
+      | Things will probably not behave as expected and there's nothing to worry about! :-)
+    p Made in Berlin in 2 weeks of Jan/2022 | with ♥ by Mariana Borssatto
 </template>
+
+<style>
+.about {
+  padding: 10rem;
+}
+
+.description {
+  padding: 2rem;
+}
+
+</style>
